5381482d8fd0292d9265c2af017722a596547fce
Amrit Khangura <amritkhangura65@gmail.com>
2022-05-26 12:47:35 -0700

    created resize button and added CSS

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const colorBtn = document.getElementById("color");
 const rainbowBtn = document.getElementById("rainbow");
 const eraserBtn = document.getElementById("eraser");
 const clearBtn = document.getElementById("clear");
-const sliderBtn = document.getElementsByClassName("slider");
+const resizeBtn = document.getElementById("resize");
+const sizeValue = document.getElementById("sizeValue");
 
 function createGrid(col, rows){
   for(let i =0; i < (col *rows); i++){
   const div = document.createElement('div');
-  div.style.border = '1px solid red';
+  // div.style.border = '1px solid red';
   grid.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${col}, 1fr)`;
   grid.appendChild(div).classList.add('box');
@@ -57,8 +58,29 @@ eraserGrid();
 
 function clearGrid() {
   const boxs = grid.querySelectorAll('.box');
-  clearBtn.addEventListener('click',() =>{
-  boxs.forEach(box => box.remove());
- } )
+  boxs.forEach(box => box.remove())
+ }
+
+
+function reSize() {
+resizeBtn.addEventListener('click', () => {
+  let user = prompt('What size would you like your grid to be');
+  if (user === null || user < 1 || user > 64) {
+    clearGrid()
+    createGrid(16,16);
+    colorMode();
+    rainbowMode()
+    eraserGrid();
+  }
+  else {
+    clearGrid()
+    createGrid(user,user);
+    colorMode();
+    rainbowMode()
+    eraserGrid();
+    sizeValue.innerHTML = `${user} x ${user}`;
+  }
+
+})
 }
-clearGrid()
+reSize()
